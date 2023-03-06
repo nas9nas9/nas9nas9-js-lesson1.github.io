d79c03af2bdc42b188a7dc0aa8e1f0baaac4247d
it-academy-logos-frontend-nas9.github.github <111902845+nas9nas9@users.noreply.github.com>
2023-03-06 19:33:05 +0200

    js-lesson1-dz

---
+++ dz/1 завдання/script.js
@@ -0,0 +1,5 @@
+let city = `Kyiv`;
+city = `Lviv`;
+let adress = city;
+
+alert(adress);
---
+++ dz/3 завдання/main.js
@@ -0,0 +1,8 @@
+const penCost = 5.25;
+const pencilCost = 3.50;
+
+const numberOfPens = 4;
+const numberOfPencils = 6;
+
+const totalCost = (penCost * numberOfPens) + (pencilCost * numberOfPencils);
+document.write(`Загальна вартість: ${totalCost} грн`);
